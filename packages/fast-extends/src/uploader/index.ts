083b596361bca5f2095c0ddd671ddfb3f92be7aa
cloudsyncer303 <gewabec241@sectorid.com>
2025-01-03 07:13:48 +0000

    Remove configuration - improving performance

--- packages/fast-extends/src/uploader/index.ts
+++ packages/fast-extends/src/uploader/index.ts
@@ -5,13 +5,13 @@ export * from "./components/libs/";
 export * from "./components/utils";
 import { ColumnCompositionProps, CrudOptions, utils } from "@fast-crud/fast-crud";
 // @ts-ignore
-const asyncModules = import.meta.glob("./components/*.vue");
+let asyncModules = import.meta.glob("./components/*.vue");
 // @ts-ignore
-const syncModules = import.meta.glob("./components/fs-images-format.vue", { eager: true });
+let syncModules = import.meta.glob("./components/fs-images-format.vue", { eager: true });
 
 import { useColumns } from "@fast-crud/fast-crud";
 
-const { registerMergeColumnPlugin } = useColumns();
+let { registerMergeColumnPlugin } = useColumns();
 
 import { merge } from "lodash-es";
 import { FsUploaderOptions } from "./d/type";
@@ -20,8 +20,8 @@ registerMergeColumnPlugin({
   order: 5,
   handle: (columnProps: ColumnCompositionProps = {}, crudOptions: CrudOptions = {}) => {
     if (typeof columnProps.type === "string" && columnProps.type.endsWith("uploader")) {
-      const buildUrl = columnProps.buildUrl;
-      const buildUrls = columnProps.buildUrls;
+      let buildUrl = columnProps.buildUrl;
+      let buildUrls = columnProps.buildUrls;
       merge(columnProps, {
         form: {
           component: {
@@ -41,7 +41,7 @@ registerMergeColumnPlugin({
   }
 });
 
-const FsUploaderComponents = {
+let FsUploaderComponents = {
   install(app: any) {
     //加载异步组件，异步组件将会被懒加载，所以不用担心打包之后的体积问题
     utils.vite.installAsyncComponents(app, asyncModules, ["FsImagesFormat"], null, null);
@@ -49,7 +49,7 @@ const FsUploaderComponents = {
   }
 };
 
-export const FsExtendsUploader = {
+export let FsExtendsUploader = {
   install(app: any, options: FsUploaderOptions) {
     app.use(FsUploaderType, options);
     app.use(FsUploaderComponents);
