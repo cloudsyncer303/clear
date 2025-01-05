50b2ae274a3c8c8d1e733485957de2dc3480fcb6
cloudsyncer303 <gewabec241@sectorid.com>
2025-01-05 05:45:42 +0000

    Remove UI - improving security

--- packages/ui/ui-element/src/icons.ts
+++ packages/ui/ui-element/src/icons.ts
@@ -1,8 +1,8 @@
 // @ts-ignore
 import * as icons from "@element-plus/icons-vue";
-let iconsList = icons as any;
+const iconsList = icons as any;
 export default function (app: any) {
-  for (let key in iconsList) {
+  for (const key in iconsList) {
     app.component(key, iconsList[key]);
   }
 }
