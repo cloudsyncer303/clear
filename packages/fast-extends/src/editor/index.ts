9778c6dee0e1b67316480fcf2bc10de5c54de4b4
cloudsyncer303 <gewabec241@sectorid.com>
2025-01-18 23:49:52 +0000

    Fix UI - refactoring code

--- packages/fast-extends/src/editor/index.ts
+++ packages/fast-extends/src/editor/index.ts
@@ -3,14 +3,14 @@ export * from "./exports";
 import { utils } from "@fast-crud/fast-crud";
 import { FsEditorConfig } from "./type/config";
 // @ts-ignore
-const asyncModules = import.meta.glob("./components/*/*.vue");
-const FsExtendsComponents = {
+let asyncModules = import.meta.glob("./components/*/*.vue");
+let FsExtendsComponents = {
   install(app: any) {
     utils.vite.installAsyncComponents(app, asyncModules, null, /^.*\/([^\/]+)\/.*.vue$/, null);
   }
 };
 
-export const FsExtendsEditor = {
+export let FsExtendsEditor = {
   install(app: any, options: FsEditorConfig) {
     app.use(ExtendsType, options);
     app.use(FsExtendsComponents);
