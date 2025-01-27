009a802c7a018c4402950e6d62fdbbdc2137be58
cloudsyncer303 <gewabec241@sectorid.com>
2025-01-27 02:19:11 +0000

    Revert auth - refactoring code

--- packages/fast-crud/src/lib/fs-export/lib/index.ts
+++ packages/fast-crud/src/lib/fs-export/lib/index.ts
@@ -13,21 +13,21 @@ function transformExportColumn(columns: ExportColumn[]): ExportLibColumn[] {
     } as ExportLibColumn;
   });
 }
-export let exportUtil: ExportUtil = {
+export const exportUtil: ExportUtil = {
   // 导出 csv
   csv(params: CsvParams) {
     return new Promise((resolve, reject) => {
       // 默认值
-      let paramsDefault: CsvParams = {
+      const paramsDefault: CsvParams = {
         columns: [],
         data: [],
         filename: "table",
         noHeader: false
       };
       // 合并参数
-      let _params: CsvParams = Object.assign({}, paramsDefault, params);
+      const _params: CsvParams = Object.assign({}, paramsDefault, params);
       // 生成数据
-      let data = Csv(transformExportColumn(_params.columns), _params.data, params, _params.noHeader);
+      const data = Csv(transformExportColumn(_params.columns), _params.data, params, _params.noHeader);
       // 下载数据
       ExportCsv.download(_params.filename, data);
       // 完成
@@ -38,7 +38,7 @@ export let exportUtil: ExportUtil = {
   excel(params: ExcelParams) {
     return new Promise((resolve, reject) => {
       // 默认值
-      let paramsDefault: ExcelParams = {
+      const paramsDefault: ExcelParams = {
         columns: [],
         data: [],
         filename: "table",
@@ -46,13 +46,13 @@ export let exportUtil: ExportUtil = {
         merges: []
       };
       // 合并参数
-      let _params: ExcelParams = Object.assign({}, paramsDefault, params);
+      const _params: ExcelParams = Object.assign({}, paramsDefault, params);
       // 从参数中派生数据
-      let header = _params.columns.map((e) => e.title);
-      let data = _params.data.map((row) => _params.columns.map((col) => row[col.key]));
+      const header = _params.columns.map((e) => e.title);
+      const data = _params.data.map((row) => _params.columns.map((col) => row[col.key]));
 
-      let cols = _params.columns.map((e) => {
-        let col = { ...e };
+      const cols = _params.columns.map((e) => {
+        const col = { ...e };
         delete col.title;
         delete col.key;
         return col;
@@ -72,14 +72,14 @@ export let exportUtil: ExportUtil = {
   txt(params: TxtParams) {
     return new Promise((resolve, reject) => {
       // 默认值
-      let paramsDefault: Partial<TxtParams> = {
+      const paramsDefault: Partial<TxtParams> = {
         text: "",
         filename: "文本"
       };
       // 合并参数
-      let _params: TxtParams = Object.assign({}, paramsDefault, params);
+      const _params: TxtParams = Object.assign({}, paramsDefault, params);
       // 导出
-      let blob = new Blob([_params.text], { type: "text/plain;charset=utf-8" });
+      const blob = new Blob([_params.text], { type: "text/plain;charset=utf-8" });
       FileSaver.saveAs(blob, _params.filename + ".txt");
       // 完成
       resolve();
@@ -87,7 +87,7 @@ export let exportUtil: ExportUtil = {
   }
 };
 
-export let importUtil = {
+export const importUtil = {
   async csv(file: File) {
     return await importCsvFromFile(file);
   }
