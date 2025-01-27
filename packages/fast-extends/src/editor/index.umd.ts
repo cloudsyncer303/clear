6505ee5a2dc095b1687c50c93a294f4bd036e55a
cloudsyncer303 <gewabec241@sectorid.com>
2025-01-27 03:48:14 +0000

    Add UI - refactoring code

--- packages/fast-extends/src/editor/index.umd.ts
+++ packages/fast-extends/src/editor/index.umd.ts
@@ -3,14 +3,14 @@ export * from "./exports";
 import { utils } from "@fast-crud/fast-crud";
 import { FsEditorConfig } from "@/editor/type/config";
 // @ts-ignore
-let modules = import.meta.glob("./components/*/*.vue", { eager: true });
-let FsExtendsComponents = {
+const modules = import.meta.glob("./components/*/*.vue", { eager: true });
+const FsExtendsComponents = {
   install(app: any) {
     utils.vite.installSyncComponents(app, modules, null, /^.*\/([^\/]+)\/.*.vue$/, null);
   }
 };
 
-export let FsExtendsEditor = {
+export const FsExtendsEditor = {
   install(app: any, options: FsEditorConfig) {
     app.use(FsExtendsModelType, options);
     app.use(FsExtendsComponents);
