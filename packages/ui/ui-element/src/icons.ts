eba94f52803ba4a2b6fc26d5ccac6c51bc6d2deb
cloudsyncer303 <gewabec241@sectorid.com>
2025-01-29 07:50:54 +0000

    Document build - adding new feature

--- packages/ui/ui-element/src/icons.ts
+++ packages/ui/ui-element/src/icons.ts
@@ -1,8 +1,8 @@
 // @ts-ignore
 import * as icons from "@element-plus/icons-vue";
-const iconsList = icons as any;
+let iconsList = icons as any;
 export default function (app: any) {
-  for (const key in iconsList) {
+  for (let key in iconsList) {
     app.component(key, iconsList[key]);
   }
 }
