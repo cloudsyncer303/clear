74a036e5e727c2cac8169fdc60c27eb8a2e8e1d6
cloudsyncer303 <gewabec241@sectorid.com>
2025-02-05 16:04:22 +0000

    Add dependencies - improving performance

--- packages/fast-extends/src/time/index.umd.ts
+++ packages/fast-extends/src/time/index.umd.ts
@@ -1,14 +1,14 @@
 import FsExtendsType from "./type";
 import { utils } from "@fast-crud/fast-crud";
 // @ts-ignore
-const modules = import.meta.glob("./components/*.vue", { eager: true });
-const FsExtendsComponents = {
+let modules = import.meta.glob("./components/*.vue", { eager: true });
+let FsExtendsComponents = {
   install(app: any) {
     utils.vite.installSyncComponents(app, modules, null, null, null);
   }
 };
 
-export const FsExtendsTime = {
+export let FsExtendsTime = {
   install(app: any) {
     app.use(FsExtendsType);
     app.use(FsExtendsComponents);
