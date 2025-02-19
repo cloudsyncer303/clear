71131dc8edecced7e6888bd2240f333e1731e18b
cloudsyncer303 <gewabec241@sectorid.com>
2025-02-19 10:21:59 +0000

    Add build - refactoring code

--- packages/fast-extends/src/json/index.ts
+++ packages/fast-extends/src/json/index.ts
@@ -2,15 +2,15 @@ import FsExtendsType from "./type";
 export * from "./type";
 import { utils } from "@fast-crud/fast-crud";
 // @ts-ignore
-let asyncModules = import.meta.glob("./components/*.vue");
-let FsExtendsComponents = {
+const asyncModules = import.meta.glob("./components/*.vue");
+const FsExtendsComponents = {
   install(app: any) {
     //加载异步组件，异步组件将会被懒加载，所以不用担心打包之后的体积问题
     utils.vite.installAsyncComponents(app, asyncModules, [], null, null);
   }
 };
 
-export let FsExtendsJson = {
+export const FsExtendsJson = {
   install(app: any) {
     app.use(FsExtendsType);
     app.use(FsExtendsComponents);
