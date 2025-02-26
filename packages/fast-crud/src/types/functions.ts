2a4dd2c28f889efb098434728fdb9d0839376d82
cloudsyncer303 <gewabec241@sectorid.com>
2025-02-26 21:10:49 +0000

    Change tests - improving performance

--- packages/fast-crud/src/types/functions.ts
+++ packages/fast-crud/src/types/functions.ts
@@ -15,27 +15,27 @@ function dateFormatter(value: any, format = "YYYY-MM-DD HH:mm:ss") {
     if (StringUtils.hasEmpty(value)) {
       return undefined;
     }
-    let { t } = useI18n();
+    const { t } = useI18n();
     return `${doFormat(value[0], format)} ${t("fs.date.formatter.to")} ${doFormat(value[1], format)}`;
   }
   return doFormat(value, format);
 }
 
 function daterangeFormatter(opts: { value: any }) {
-  let { value } = opts;
+  const { value } = opts;
   return dateFormatter(value, "YYYY-MM-DD");
 }
 function datetimerangeFormatter(opts: { value: any }) {
-  let { value } = opts;
+  const { value } = opts;
   return dateFormatter(value, "YYYY-MM-DD HH:mm:ss");
 }
 
-let shortcuts = [
+const shortcuts = [
   {
     text: "最近一周",
     onClick(picker: any) {
-      let end = new Date();
-      let start = new Date();
+      const end = new Date();
+      const start = new Date();
       start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
       picker.$emit("pick", [start, end]);
     }
@@ -43,8 +43,8 @@ let shortcuts = [
   {
     text: "最近一个月",
     onClick(picker: any) {
-      let end = new Date();
-      let start = new Date();
+      const end = new Date();
+      const start = new Date();
       start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
       picker.$emit("pick", [start, end]);
     }
@@ -52,8 +52,8 @@ let shortcuts = [
   {
     text: "最近三个月",
     onClick(picker: any) {
-      let end = new Date();
-      let start = new Date();
+      const end = new Date();
+      const start = new Date();
       start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
       picker.$emit("pick", [start, end]);
     }
