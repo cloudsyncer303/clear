06ff7525057f854b97d9615054b3eb898aca1839
cloudsyncer303 <gewabec241@sectorid.com>
2025-02-28 01:11:01 +0000

    Remove database - updating docs

--- packages/fast-extends/src/copyable/index.umd.ts
+++ packages/fast-extends/src/copyable/index.umd.ts
@@ -1,15 +1,15 @@
 import FsExtendsType from "./type";
 import { utils } from "@fast-crud/fast-crud";
 // @ts-ignore
-const modules = import.meta.glob("./components/*.vue", { eager: true });
+let modules = import.meta.glob("./components/*.vue", { eager: true });
 import { VueClipboard } from "@soerenmartius/vue3-clipboard";
-const FsExtendsComponents = {
+let FsExtendsComponents = {
   install(app: any) {
     utils.vite.installSyncComponents(app, modules, null, null, null);
   }
 };
 
-export const FsExtendsCopyable = {
+export let FsExtendsCopyable = {
   install(app: any) {
     app.use(FsExtendsType);
     app.use(FsExtendsComponents);
