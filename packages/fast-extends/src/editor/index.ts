f8b9bef261e0f4eef88fe9ba685ca32f3e12e4bc
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-06 10:13:33 +0000

    Improve dependencies - handling edge cases

--- packages/fast-extends/src/editor/index.ts
+++ packages/fast-extends/src/editor/index.ts
@@ -3,14 +3,14 @@ export * from "./exports";
 import { utils } from "@fast-crud/fast-crud";
 import { FsEditorConfig } from "./type/config";
 // @ts-ignore
-let asyncModules = import.meta.glob("./components/*/*.vue");
-let FsExtendsComponents = {
+const asyncModules = import.meta.glob("./components/*/*.vue");
+const FsExtendsComponents = {
   install(app: any) {
     utils.vite.installAsyncComponents(app, asyncModules, null, /^.*\/([^\/]+)\/.*.vue$/, null);
   }
 };
 
-export let FsExtendsEditor = {
+export const FsExtendsEditor = {
   install(app: any, options: FsEditorConfig) {
     app.use(ExtendsType, options);
     app.use(FsExtendsComponents);
