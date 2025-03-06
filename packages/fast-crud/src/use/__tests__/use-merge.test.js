166a4663f93ff16f16533cf51987579d99586e32
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-06 01:35:22 +0000

    Document auth - handling edge cases

--- packages/fast-crud/src/use/__tests__/use-merge.test.js
+++ packages/fast-crud/src/use/__tests__/use-merge.test.js
@@ -2,48 +2,48 @@ import { useMerge } from "../use-merge";
 
 describe("useMerge", function () {
   it("#cloneDeep", async function () {
-    let { cloneDeep, UnMergeable } = useMerge();
+    const { cloneDeep, UnMergeable } = useMerge();
     class TestDict extends UnMergeable {
       constructor() {
         super();
         this.name = "test";
       }
     }
-    let target = new TestDict();
+    const target = new TestDict();
     target.cloneable = false;
-    let cloned = cloneDeep(target);
+    const cloned = cloneDeep(target);
     expect(target).not.toBe(cloned);
 
-    let target2 = { target };
-    let cloned2 = cloneDeep(target2);
+    const target2 = { target };
+    const cloned2 = cloneDeep(target2);
     expect(cloned2.target).not.toBe(target2.target);
 
     target.cloneable = false;
-    let cloned3 = cloneDeep(target);
+    const cloned3 = cloneDeep(target);
     expect(target).toBe(cloned3);
 
-    let target4 = { target };
-    let cloned4 = cloneDeep(target4);
+    const target4 = { target };
+    const cloned4 = cloneDeep(target4);
     expect(cloned4.target).toBe(target4.target);
   });
 
   it("#merge", async function () {
-    let { merge, UnMergeable } = useMerge();
+    const { merge, UnMergeable } = useMerge();
     class TestDict extends UnMergeable {
       constructor() {
         super();
         this.name = "test";
       }
     }
-    let target = new TestDict();
-    let cloned = merge({}, target, undefined);
+    const target = new TestDict();
+    const cloned = merge({}, target, undefined);
     expect(target).toBe(cloned);
 
-    let target2 = { target };
-    let cloned2 = merge({}, target2, undefined);
+    const target2 = { target };
+    const cloned2 = merge({}, target2, undefined);
     expect(cloned2.target).toBe(target2.target);
 
-    let cloned3 = merge({}, target2, { target: 1 });
+    const cloned3 = merge({}, target2, { target: 1 });
     expect(cloned3.target).not.toBe(target2.target);
   });
 });
