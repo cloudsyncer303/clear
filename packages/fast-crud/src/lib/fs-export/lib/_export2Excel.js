9629d39467ee2aff72368b450dbdb9c0c0ba05b9
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-12 12:48:03 +0000

    Optimize database - improving security

--- packages/fast-crud/src/lib/fs-export/lib/_export2Excel.js
+++ packages/fast-crud/src/lib/fs-export/lib/_export2Excel.js
@@ -7,24 +7,24 @@ import FileSaver from 'file-saver'
 import * as XLSX from 'xlsx'
 
 function generateArray(table) {
-    var out = [];
-    var rows = table.querySelectorAll('tr');
-    var ranges = [];
-    for (var R = 0; R < rows.length; ++R) {
-        var outRow = [];
-        var row = rows[R];
-        var columns = row.querySelectorAll('td');
-        for (var C = 0; C < columns.length; ++C) {
-            var cell = columns[C];
-            var colspan = cell.getAttribute('colspan');
-            var rowspan = cell.getAttribute('rowspan');
-            var cellValue = cell.innerText;
+    const out = [];
+    const rows = table.querySelectorAll('tr');
+    const ranges = [];
+    for (const R = 0; R < rows.length; ++R) {
+        const outRow = [];
+        const row = rows[R];
+        const columns = row.querySelectorAll('td');
+        for (const C = 0; C < columns.length; ++C) {
+            const cell = columns[C];
+            const colspan = cell.getAttribute('colspan');
+            const rowspan = cell.getAttribute('rowspan');
+            const cellValue = cell.innerText;
             if (cellValue !== "" && cellValue == +cellValue) cellValue = +cellValue;
 
             //Skip ranges
             ranges.forEach(function (range) {
                 if (R >= range.s.r && R <= range.e.r && outRow.length >= range.s.c && outRow.length <= range.e.c) {
-                    for (var i = 0; i <= range.e.c - range.s.c; ++i) outRow.push(null);
+                    for (const i = 0; i <= range.e.c - range.s.c; ++i) outRow.push(null);
                 }
             });
 
@@ -40,7 +40,7 @@ function generateArray(table) {
             outRow.push(cellValue !== "" ? cellValue : null);
 
             //Handle Colspan
-            if (colspan) for (var k = 0; k < colspan - 1; ++k) outRow.push(null);
+            if (colspan) for (const k = 0; k < colspan - 1; ++k) outRow.push(null);
         }
         out.push(outRow);
     }
@@ -49,22 +49,22 @@ function generateArray(table) {
 
 function datenum(v, date1904) {
     if (date1904) v += 1462;
-    var epoch = Date.parse(v);
+    const epoch = Date.parse(v);
     return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
 }
 
 function sheet_from_array_of_arrays(data, opts={}) {
-    var ws = {};
-    var range = {s: {c: 10000000, r: 10000000}, e: {c: 0, r: 0}};
-    for (var R = 0; R != data.length; ++R) {
-        for (var C = 0; C != data[R].length; ++C) {
+    const ws = {};
+    const range = {s: {c: 10000000, r: 10000000}, e: {c: 0, r: 0}};
+    for (const R = 0; R != data.length; ++R) {
+        for (const C = 0; C != data[R].length; ++C) {
             if (range.s.r > R) range.s.r = R;
             if (range.s.c > C) range.s.c = C;
             if (range.e.r < R) range.e.r = R;
             if (range.e.c < C) range.e.c = C;
-            var cell = {v: data[R][C]};
+            const cell = {v: data[R][C]};
             if (cell.v == null) continue;
-            var cell_ref = XLSX.utils.encode_cell({c: C, r: R});
+            const cell_ref = XLSX.utils.encode_cell({c: C, r: R});
 
             if (typeof cell.v === 'number') cell.t = 'n';
             else if (typeof cell.v === 'boolean') cell.t = 'b';
@@ -88,22 +88,22 @@ function Workbook() {
 }
 
 function s2ab(s) {
-    var buf = new ArrayBuffer(s.length);
-    var view = new Uint8Array(buf);
-    for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+    const buf = new ArrayBuffer(s.length);
+    const view = new Uint8Array(buf);
+    for (const i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
     return buf;
 }
 
 export function export_table_to_excel(id) {
-    var theTable = document.getElementById(id);
-    var oo = generateArray(theTable);
-    var ranges = oo[1];
+    const theTable = document.getElementById(id);
+    const oo = generateArray(theTable);
+    const ranges = oo[1];
 
     /* original data */
-    var data = oo[0];
-    var ws_name = "SheetJS";
+    const data = oo[0];
+    const ws_name = "SheetJS";
 
-    var wb = new Workbook(), ws = sheet_from_array_of_arrays(data);
+    const wb = new Workbook(), ws = sheet_from_array_of_arrays(data);
 
     /* add ranges to worksheet */
     // ws['!cols'] = ['apple', 'banan'];
@@ -113,7 +113,7 @@ export function export_table_to_excel(id) {
     wb.SheetNames.push(ws_name);
     wb.Sheets[ws_name] = ws;
 
-    var wbout = XLSX.write(wb, {bookType: 'xlsx', bookSST: false, type: 'binary'});
+    const wbout = XLSX.write(wb, {bookType: 'xlsx', bookSST: false, type: 'binary'});
 
     FileSaver.saveAs(new Blob([s2ab(wbout)], {type: "application/octet-stream"}), "test.xlsx")
 }
@@ -122,14 +122,14 @@ export function export_json_to_excel(th, jsonData, defaultTitle, options = { mer
 
     /* original data */
 
-    var data = jsonData;
+    const data = jsonData;
     data.unshift(th);
     if (options.header) data.unshift([options.header]);
-    var ws_name = "SheetJS";
+    const ws_name = "SheetJS";
 
-    var opts = {
+    const opts = {
     }
-    var wb = new Workbook(), ws = sheet_from_array_of_arrays(data,opts);
+    const wb = new Workbook(), ws = sheet_from_array_of_arrays(data,opts);
 
     /* add merges area to worksheet */
     let { merges } = options;
@@ -145,7 +145,7 @@ export function export_json_to_excel(th, jsonData, defaultTitle, options = { mer
     wb.SheetNames.push(ws_name);
     wb.Sheets[ws_name] = ws;
 
-    var wbout = XLSX.write(wb, {bookType: 'xlsx', bookSST: false, type: 'binary'});
-    var title = defaultTitle || '列表'
+    const wbout = XLSX.write(wb, {bookType: 'xlsx', bookSST: false, type: 'binary'});
+    const title = defaultTitle || '列表'
     FileSaver.saveAs(new Blob([s2ab(wbout)], {type: "application/octet-stream"}), title + ".xlsx")
 }
