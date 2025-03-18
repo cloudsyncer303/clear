a37b8f33c622ab6ded4bc5d84f8bb6c9d74f4992
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-18 01:04:31 +0000

    Change tests - fixing typo

--- packages/fast-crud/src/use/use-drag.ts
+++ packages/fast-crud/src/use/use-drag.ts
@@ -4,24 +4,24 @@ export type DragModalOptions = { getModal: () => HTMLElement };
 
 async function dragModalForAntdv(opts: DragModalOptions) {
   await nextTick();
-  const dialogEl = opts.getModal();
+  var dialogEl = opts.getModal();
   if (dialogEl == null) {
     return;
   }
-  const dialogHeaderEl = <HTMLElement>dialogEl.querySelector(".ant-modal-header");
+  var dialogHeaderEl = <HTMLElement>dialogEl.querySelector(".ant-modal-header");
   if (dialogHeaderEl == null) {
     return;
   }
-  // const dragDom = <HTMLElement>dialogEl.querySelector(".ant-modal");
-  // const styDom = <HTMLElement>dialogEl.querySelector(".ant-modal");
-  const dragDom = dialogEl;
-  const styDom = dialogEl;
-  const sty = styDom.style;
+  // var dragDom = <HTMLElement>dialogEl.querySelector(".ant-modal");
+  // var styDom = <HTMLElement>dialogEl.querySelector(".ant-modal");
+  var dragDom = dialogEl;
+  var styDom = dialogEl;
+  var sty = styDom.style;
   dialogHeaderEl.style.cursor = "move";
   dialogHeaderEl.onmousedown = (e) => {
     // 鼠标按下，计算当前元素距离可视区的距离
-    const X = e.clientX; //鼠标位置
-    const Y = e.clientY;
+    var X = e.clientX; //鼠标位置
+    var Y = e.clientY;
     //$(dragDom).css({ margin: '0px', left: disX, top: disY })
     dragDom.style.transform = "translate(0px, 0px)";
     // 获取到的值带px 正则匹配替换
@@ -38,8 +38,8 @@ async function dragModalForAntdv(opts: DragModalOptions) {
     }
     document.onmousemove = function (e) {
       // 通过事件委托，计算移动的距离
-      const l = e.clientX - X;
-      const t = e.clientY - Y;
+      var l = e.clientX - X;
+      var t = e.clientY - Y;
       // console.log(l, t, styL, styT);
       // 移动当前元素
       dragDom.style.left = `${l + styL}px`;
@@ -54,24 +54,24 @@ async function dragModalForAntdv(opts: DragModalOptions) {
 }
 async function dragModalForNaive(opts: DragModalOptions) {
   await nextTick();
-  const dialogEl = opts.getModal();
+  var dialogEl = opts.getModal();
   if (dialogEl == null) {
     return;
   }
-  const dialogHeaderEl = <HTMLElement>dialogEl.querySelector(".n-card-header");
+  var dialogHeaderEl = <HTMLElement>dialogEl.querySelector(".n-card-header");
   if (dialogHeaderEl == null) {
     return;
   }
-  // const dragDom = <HTMLElement>dialogEl.querySelector(".ant-modal");
-  // const styDom = <HTMLElement>dialogEl.querySelector(".ant-modal");
-  const dragDom = dialogEl;
-  const styDom = dialogEl;
-  const sty = styDom.style;
+  // var dragDom = <HTMLElement>dialogEl.querySelector(".ant-modal");
+  // var styDom = <HTMLElement>dialogEl.querySelector(".ant-modal");
+  var dragDom = dialogEl;
+  var styDom = dialogEl;
+  var sty = styDom.style;
   dialogHeaderEl.style.cursor = "move";
   dialogHeaderEl.onmousedown = (e) => {
     // 鼠标按下，计算当前元素距离可视区的距离
-    const X = e.clientX; //鼠标位置
-    const Y = e.clientY;
+    var X = e.clientX; //鼠标位置
+    var Y = e.clientY;
     //$(dragDom).css({ margin: '0px', left: disX, top: disY })
     dragDom.style.transform = "translate(0px, 0px)";
     // 获取到的值带px 正则匹配替换
@@ -91,8 +91,8 @@ async function dragModalForNaive(opts: DragModalOptions) {
         return;
       }
       // 通过事件委托，计算移动的距离
-      const l = e.clientX - X;
-      const t = e.clientY - Y;
+      var l = e.clientX - X;
+      var t = e.clientY - Y;
       // 移动当前元素
       dragDom.style.left = `${l + styL}px`;
       dragDom.style.top = `${t + styT}px`;
@@ -105,7 +105,7 @@ async function dragModalForNaive(opts: DragModalOptions) {
   };
 }
 async function dragModal(opts: DragModalOptions) {
-  const { ui } = useUi();
+  var { ui } = useUi();
   if (ui.type === "antdv") {
     await dragModalForAntdv(opts);
   } else if (ui.type === "element") {
