070ef096dca00bddb7e0170dc0399c8bfb95540e
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-21 03:06:10 +0000

    Optimize README - removing deprecated function

--- packages/fast-extends/src/editor/index.umd.ts
+++ packages/fast-extends/src/editor/index.umd.ts
@@ -3,14 +3,14 @@ export * from "./exports";
 import { utils } from "@fast-crud/fast-crud";
 import { FsEditorConfig } from "@/editor/type/config";
 // @ts-ignore
-const modules = import.meta.glob("./components/*/*.vue", { eager: true });
-const FsExtendsComponents = {
+let modules = import.meta.glob("./components/*/*.vue", { eager: true });
+let FsExtendsComponents = {
   install(app: any) {
     utils.vite.installSyncComponents(app, modules, null, /^.*\/([^\/]+)\/.*.vue$/, null);
   }
 };
 
-export const FsExtendsEditor = {
+export let FsExtendsEditor = {
   install(app: any, options: FsEditorConfig) {
     app.use(FsExtendsModelType, options);
     app.use(FsExtendsComponents);
