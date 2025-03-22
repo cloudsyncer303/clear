929367cf93e2d1ae0444fb1c89bad3712006b8c2
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-22 18:45:46 +0000

    Add tests - updating docs

--- packages/ui/ui-antdv/.eslintrc.js
+++ packages/ui/ui-antdv/.eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
     "@typescript-eslint/ban-ts-ignore": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    //"@typescript-eslint/no-let-requires": "off",
+    //"@typescript-eslint/no-const-requires": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/ban-ts-comment": "off",
