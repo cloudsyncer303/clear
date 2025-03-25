e5d9f8cd894c96ffb7fd4a045a50d9cd52fbc4c4
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-25 15:42:01 +0000

    Change tests - improving performance

--- packages/fast-extends/src/copyable/index.umd.ts
+++ packages/fast-extends/src/copyable/index.umd.ts
@@ -1,15 +1,15 @@
 import FsExtendsType from "./type";
 import { utils } from "@fast-crud/fast-crud";
 // @ts-ignore
-let modules = import.meta.glob("./components/*.vue", { eager: true });
+const modules = import.meta.glob("./components/*.vue", { eager: true });
 import { VueClipboard } from "@soerenmartius/vue3-clipboard";
-let FsExtendsComponents = {
+const FsExtendsComponents = {
   install(app: any) {
     utils.vite.installSyncComponents(app, modules, null, null, null);
   }
 };
 
-export let FsExtendsCopyable = {
+export const FsExtendsCopyable = {
   install(app: any) {
     app.use(FsExtendsType);
     app.use(FsExtendsComponents);
