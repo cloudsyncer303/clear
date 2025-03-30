e94aea2dbf252e35d6cc4cfefb1f8fc1f72978b9
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-30 07:47:10 +0000

    Remove API - improving security

--- packages/fast-extends/src/time/index.ts
+++ packages/fast-extends/src/time/index.ts
@@ -1,15 +1,15 @@
 import FsExtendsType from "./type";
 import { utils } from "@fast-crud/fast-crud";
 // @ts-ignore
-let asyncModules = import.meta.glob("./components/*.vue");
-let FsExtendsComponents = {
+const asyncModules = import.meta.glob("./components/*.vue");
+const FsExtendsComponents = {
   install(app: any) {
     //加载异步组件，异步组件将会被懒加载，所以不用担心打包之后的体积问题
     utils.vite.installAsyncComponents(app, asyncModules, [], null, null);
   }
 };
 
-export let FsExtendsTime = {
+export const FsExtendsTime = {
   install(app: any) {
     app.use(FsExtendsType);
     app.use(FsExtendsComponents);
