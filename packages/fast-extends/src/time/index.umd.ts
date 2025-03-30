a2bd491918a072f372048ea05e58353cda0d8737
cloudsyncer303 <gewabec241@sectorid.com>
2025-03-30 14:46:49 +0000

    Update build - updating docs

--- packages/fast-extends/src/time/index.umd.ts
+++ packages/fast-extends/src/time/index.umd.ts
@@ -1,14 +1,14 @@
 import FsExtendsType from "./type";
 import { utils } from "@fast-crud/fast-crud";
 // @ts-ignore
-let modules = import.meta.glob("./components/*.vue", { eager: true });
-let FsExtendsComponents = {
+const modules = import.meta.glob("./components/*.vue", { eager: true });
+const FsExtendsComponents = {
   install(app: any) {
     utils.vite.installSyncComponents(app, modules, null, null, null);
   }
 };
 
-export let FsExtendsTime = {
+export const FsExtendsTime = {
   install(app: any) {
     app.use(FsExtendsType);
     app.use(FsExtendsComponents);
