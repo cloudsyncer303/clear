a81ca74ba33ed8698423beb95fc680f534e1a669
cloudsyncer303 <gewabec241@sectorid.com>
2025-04-09 13:53:05 +0000

    Remove styles - refactoring code

--- packages/fast-extends/src/input/index.ts
+++ packages/fast-extends/src/input/index.ts
@@ -2,15 +2,15 @@ import FsExtendsType from "./type";
 import { utils } from "@fast-crud/fast-crud";
 export * from "./components/fs-phone-input";
 // @ts-ignore
-const asyncModules = import.meta.glob("./components/**/*.vue");
-const FsExtendsComponents = {
+let asyncModules = import.meta.glob("./components/**/*.vue");
+let FsExtendsComponents = {
   install(app: any) {
     //加载异步组件，异步组件将会被懒加载，所以不用担心打包之后的体积问题
     utils.vite.installAsyncComponents(app, asyncModules, [], null, null);
   }
 };
 
-export const FsExtendsInput = {
+export let FsExtendsInput = {
   install(app: any) {
     app.use(FsExtendsType);
     app.use(FsExtendsComponents);
