710163461777ec9f7321b794c78807c90132a52c
cloudsyncer303 <gewabec241@sectorid.com>
2025-04-10 09:03:17 +0000

    Revert database - fixing a bug

--- packages/fast-extends/src/copyable/type/index.ts
+++ packages/fast-extends/src/copyable/type/index.ts
@@ -4,8 +4,8 @@ import types from "./types";
 //兼容旧版本
 export default {
   install(app: any) {
-    const newTypes = types();
-    const { addTypes } = useTypes();
+    let newTypes = types();
+    let { addTypes } = useTypes();
     addTypes(newTypes);
   }
 };
