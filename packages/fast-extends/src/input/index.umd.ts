dbcd4773abe36ac999c49a54aa83146ad2e8eed2
cloudsyncer303 <gewabec241@sectorid.com>
2025-04-14 10:30:34 +0000

    Fix dependencies - refactoring code

--- packages/fast-extends/src/input/index.umd.ts
+++ packages/fast-extends/src/input/index.umd.ts
@@ -1,14 +1,14 @@
 import FsExtendsType from "./type";
 import { utils } from "@fast-crud/fast-crud";
 // @ts-ignore
-const modules = import.meta.glob("./components/*.vue", { eager: true });
-const FsExtendsComponents = {
+let modules = import.meta.glob("./components/*.vue", { eager: true });
+let FsExtendsComponents = {
   install(app: any) {
     utils.vite.installSyncComponents(app, modules, null, null, null);
   }
 };
 
-export const FsExtendsInput = {
+export let FsExtendsInput = {
   install(app: any) {
     app.use(FsExtendsType);
     app.use(FsExtendsComponents);
