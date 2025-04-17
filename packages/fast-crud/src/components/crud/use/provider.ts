8d63c81bb1e25994ab45e9f9bb6fb536101dbdb6
cloudsyncer303 <gewabec241@sectorid.com>
2025-04-17 18:01:46 +0000

    Remove auth - handling edge cases

--- packages/fast-crud/src/components/crud/use/provider.ts
+++ packages/fast-crud/src/components/crud/use/provider.ts
@@ -2,11 +2,11 @@ import { provide } from "vue";
 import { inject, Ref } from "vue";
 import { get, set } from "lodash-es";
 export function useComponentRefProvider(componentsRef: Ref) {
-  let getter = (index: number, key: string) => {
+  const getter = (index: number, key: string) => {
     return get(componentsRef, `value[${index}].${key}`);
   };
   provide("componentRef:get", getter);
-  let setter = (index: number, key: string, value: any) => {
+  const setter = (index: number, key: string, value: any) => {
     return set(componentsRef, `value[${index}].${key}`, value);
   };
   provide("componentRef:set", setter);
@@ -18,8 +18,8 @@ export function useComponentRefProvider(componentsRef: Ref) {
 }
 
 export function useComponentRefInject() {
-  let getter = inject("componentRef:get");
-  let setter = inject("componentRef:set");
+  const getter = inject("componentRef:get");
+  const setter = inject("componentRef:set");
   return {
     getter,
     setter
