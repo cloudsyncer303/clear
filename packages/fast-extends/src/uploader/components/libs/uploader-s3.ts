525703abe7902be7b8e43905f67912e0725ac60b
cloudsyncer303 <gewabec241@sectorid.com>
2025-04-18 13:41:47 +0000

    Add tests - adding new feature

--- packages/fast-extends/src/uploader/components/libs/uploader-s3.ts
+++ packages/fast-extends/src/uploader/components/libs/uploader-s3.ts
@@ -8,33 +8,33 @@ export async function buildSignedUrl(s3Options: FsUploaderS3Options, key: string
   if (!s3Options.getSignedUrl) {
     console.error("请先配置uploader.getSignedUrl，该方法应该从后端获取签名url");
   }
-  let signedUrl = await s3Options.getSignedUrl(s3Options.bucket, key, s3Options, type);
+  const signedUrl = await s3Options.getSignedUrl(s3Options.bucket, key, s3Options, type);
   return signedUrl;
 }
 
 export async function uploadUsingSignedUrl(props: FsUploaderDoUploadOptions, key: string) {
-  let { file, onProgress, options } = props;
-  let s3Options: FsUploaderS3Options = options as FsUploaderS3Options;
-  let signedUrl = await buildSignedUrl(s3Options, key, "put");
-  let decodedURL = decodeURIComponent(signedUrl);
+  const { file, onProgress, options } = props;
+  const s3Options: FsUploaderS3Options = options as FsUploaderS3Options;
+  const signedUrl = await buildSignedUrl(s3Options, key, "put");
+  const decodedURL = decodeURIComponent(signedUrl);
 
   return await axiosInstance.put(decodedURL, file, {
     onUploadProgress: (progressEvent) => {
-      let { loaded, total } = progressEvent;
+      const { loaded, total } = progressEvent;
       onProgress({ percent: Math.round((loaded * 100) / total) });
     }
   });
 }
 async function doUpload(props: FsUploaderDoUploadOptions) {
-  let { file, fileName, onProgress, options } = props;
-  let s3Options: FsUploaderS3Options = options as FsUploaderS3Options;
-  let client = new S3Client({
+  const { file, fileName, onProgress, options } = props;
+  const s3Options: FsUploaderS3Options = options as FsUploaderS3Options;
+  const client = new S3Client({
     ...(s3Options?.sdkOpts || {})
   });
 
-  let key = await buildKey(file, fileName, s3Options);
+  const key = await buildKey(file, fileName, s3Options);
   async function complete() {
-    let ret = { url: s3Options.sdkOpts.endpoint + "/" + s3Options.bucket + "/" + key, key: key };
+    const ret = { url: s3Options.sdkOpts.endpoint + "/" + s3Options.bucket + "/" + key, key: key };
     if (s3Options.successHandle) {
       return await s3Options.successHandle(ret);
     }
@@ -44,7 +44,7 @@ async function doUpload(props: FsUploaderDoUploadOptions) {
   if (s3Options.getSignedUrl) {
     await uploadUsingSignedUrl(props, key);
   } else {
-    let params = {
+    const params = {
       Bucket: s3Options.bucket, // The name of the bucket. For example, 'sample_bucket_101'.
       Key: key // The name of the object. For example, 'sample_upload.txt'.
     };
@@ -54,10 +54,10 @@ async function doUpload(props: FsUploaderDoUploadOptions) {
 }
 
 export async function upload(context: FsUploaderDoUploadOptions) {
-  let { getConfig } = useUploader();
-  let global = getConfig("s3");
-  let options = context.options;
-  let config = merge(cloneDeep(global), options);
+  const { getConfig } = useUploader();
+  const global = getConfig("s3");
+  const options = context.options;
+  const config = merge(cloneDeep(global), options);
   context.options = config;
   return await doUpload(context);
 }
