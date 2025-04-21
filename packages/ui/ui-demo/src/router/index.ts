381a35e19579f5d8a18e44a295517929437ea167
cloudsyncer303 <gewabec241@sectorid.com>
2025-04-21 06:11:15 +0000

    Fix auth - adding new feature

--- packages/ui/ui-demo/src/router/index.ts
+++ packages/ui/ui-demo/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-let router = createRouter({
+const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
